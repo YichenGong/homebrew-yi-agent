6ffec88a4192e79213da0a7b2018fff4fbec7c8b
YichenGong <YichenGong@users.noreply.github.com>
2026-07-19 18:18:55 +0800

    Update yi-agent to 0.1.0

--- Formula/yi-agent.rb
+++ Formula/yi-agent.rb
@@ -8,17 +8,17 @@ class YiAgent < Formula
   on_macos do
     on_intel do
       url "https://github.com/YichenGong/yi-agent/releases/download/v0.1.0/yi-agent-x86_64-apple-darwin.tar.gz"
-      sha256 "558bbfa1e0391ca57705cd8749a3319b5df86b90c0e04e90f0ccd697b380299b"
+      sha256 "18e969abe55c5aa61361473f48dcb1bb0e806eaa0df0cd9e5426e130c52c2d62"
     end
     on_arm do
       url "https://github.com/YichenGong/yi-agent/releases/download/v0.1.0/yi-agent-aarch64-apple-darwin.tar.gz"
-      sha256 "b94bc3391b35dd1b356ca4385d038948f7ed627a06d7744070d33d74e2e05bc2"
+      sha256 "4e94efec34687bbb28342a11781fb58655db0255c87d408c9849111f3296238d"
     end
   end
 
   on_linux do
     url "https://github.com/YichenGong/yi-agent/releases/download/v0.1.0/yi-agent-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "4973e7edfb19525e914f874adbf8d398caa1d5de127ca34dae7a513ccd51c7c6"
+    sha256 "a7afcd9cc0baa42b1097a725a06b1bb292a4c2bb66615c5a1994570ea34051b4"
   end
 
   def install
